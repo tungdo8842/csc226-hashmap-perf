27734b5d06809e3caa265d104f828f06ef4edfc1
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2026-02-22 13:04:38 -0800

    Finish access_time test

--- access_time.c
+++ access_time.c
@@ -2,32 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ARRAY_LENGTH 1000 * 1000
+#define ARRAY_LENGTH 100
 #define LOOP_AMOUNT 1000 * 1000 * 1000
-#define ACCESS_INDEX 0
+#define ACCESS_INDEX 5
 
 typedef struct ListNode {
     int value;
     struct ListNode *next;
 } ListNode;
 
-void benchmark_array(int *arr, int index);
-void benchmark_linked_list(ListNode *list, int index);
-
 int main() {
     clock_t begin = clock();
+
     // initialize array and linked list
     int *arr = (int*) malloc(sizeof(int) * ARRAY_LENGTH);
-
-    // initialize linked list and value of both linked list and array
     ListNode *list = (ListNode*) malloc(sizeof(ListNode));
-    list->value = 0;
-    ListNode *current_node = list;
 
+    // initialize value of both linked list and array
+
+    ListNode *list_arr[4] = {list, list, list, list};
     for (int i = 0; i < ARRAY_LENGTH; i++) {
         arr[i] = i;
     }
-
+    list->value = 0;
+    ListNode *current_node = list;
     for (int i = 0; i < ARRAY_LENGTH-1; i++) {
         ListNode *new_node = (ListNode*) malloc(sizeof(ListNode));
         new_node->value = i + 1;
@@ -39,12 +37,14 @@ int main() {
     double total_time = (double)(end-begin) / CLOCKS_PER_SEC;
     printf("initialization time: %f\n", total_time);
 
+
     printf("Testing element access time of index %d with %d loops\n", ACCESS_INDEX, LOOP_AMOUNT);
 
     // test array access time
     begin = clock();
+    int value_arr = 0;
     for (int i = 0; i < LOOP_AMOUNT; i++) {
-        int value_arr = arr[ACCESS_INDEX];
+        value_arr = arr[ACCESS_INDEX];
     }
     end = clock();
     total_time = (double)(end-begin) / CLOCKS_PER_SEC;
@@ -52,9 +52,13 @@ int main() {
 
     // test linked list access time
     begin = clock();
-    current_node = list;
+    int value_linked_list = 0;
     for (int i = 0; i < LOOP_AMOUNT; i++) {
-        benchmark_linked_list(list, ACCESS_INDEX);
+        current_node = list_arr[3];
+        for (int j = 0; j < ACCESS_INDEX-1; j++) {
+            value_linked_list = current_node->value;
+            current_node = current_node->next;    
+        }
     }
     end = clock();
     total_time = (double)(end-begin) / CLOCKS_PER_SEC;
@@ -68,16 +72,4 @@ int main() {
         current_node = current_node->next;
         free(free_node);
     }
-}
-
-void benchmark_array(int *arr, int index){
-    int value_arr = arr[index];
-}
-
-void benchmark_linked_list(ListNode *list, int index){
-    ListNode *current_node = list;
-    for (int i = 0; i < index; i++) {
-        current_node = current_node->next;    
-    }
-    int value_linked_list = current_node->value;
 }
